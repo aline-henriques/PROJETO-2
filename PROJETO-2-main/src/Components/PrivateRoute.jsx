ffa87ab78e85e4a331b43b7f6deca2021085020e
Bruno-cod <brunokami1209@gmail.com>
2025-06-02 18:50:27 -0300

    Trabalho Final 2

--- PROJETO-2-main/src/Components/PrivateRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-export const PrivateRoute = () => {
-    const isAuthenticated = localStorage.getItem('isLoggedIn') || sessionStorage.getItem('isLoggedIn');
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-};
