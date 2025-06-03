c8704570bef99fdc479a38672bef205a4ef5ec6f
Aline Henriques <alinealbhenriques@gmail.com>
2025-06-03 17:03:33 -0300

    .

--- src/Routes/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../Services/AuthContext';
-
-export const PrivateRoute = () => {
-    const {isAuthenticated} = useAuth();
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-};
